498484111a4dd87f78972027990464d1273c1041
Hamdy248 <135514777+Hamdy248@users.noreply.github.com>
2023-07-08 11:53:57 +0300

    first commit

---
+++ lib/Features/Splash/presentation/views/splash_view.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: unnecessary_import, implementation_imports
+
+import 'package:bookly/Features/Splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewbody(),
+    );
+  }
+}
---
+++ lib/core/utils/function/launch_url.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'custom_snack_bar.dart';
+
+Future<void> launchCustomUr(context, String? url) async {
+  if (url != null) {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      customSnackBar(context, 'Cannot launch $url');
+    }
+  }
+}
